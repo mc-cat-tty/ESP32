42c6a47ea6c0bae087614e58d06fb3612c2d2989
mc-cat-tty <>
2021-01-05 12:28:42 +0100

    naive_stopwatch.py works

--- MicroPython/naive_stopwatch.py
+++ MicroPython/naive_stopwatch.py
@@ -22,12 +22,15 @@ class State:
 LED_PIN: int = 21  # Change this value, it may be different for your board
 BUTTON_PIN: int = 0  # Change this value, it may be different for your board
 CENTS: int = 0
-FMS_STATE: State = State.IDLE
+COUNTER_RUNNING: bool = False
 
 def printTime() -> None:
     global CENTS
     t = utime.localtime(int(CENTS/100))
-    print("Time: %02d:%02d:%02d" % (t[3], t[4], t[5]), end="\r")
+    print("\rTime: %02d:%02d:%02d" % (t[3], t[4], t[5]), end="")
+
+def printAddCents() -> None:  # Add centiseconds to the previous timestamp
+    print(".%02d" % (CENTS%100), end="")
 
 async def counterCoro() -> None:
     global CENTS
@@ -38,57 +41,71 @@ async def counterCoro() -> None:
             printTime()
         await uasyncio.sleep_ms(10)  # 10 ms, namely 1 cs
 
-async def buttonTask(button: Pin, led: Pin) -> None:
-    global FMS_STATE
+async def buttonCoro(button: Pin, led: Pin) -> None:
+    global CENTS, COUNTER_RUNNING
     print("Entered buttonTask()")
+    print("Start stopwatch by pressing BOOT button")
     startCents: int = 0
+    fms_state: State = State.IDLE
+    counterCoroObj: uasyncio.Task = None
     while True:
-        buttonValue = button.value
-        if FMS_STATE is State.IDLE:
+        buttonValue = button.value()
+        if fms_state == State.IDLE:
             led.off()
             if buttonValue == 0:  # Pressed
                 startCents = CENTS
-                FMS_STATE = State.LAP
+                fms_state = State.LAP
             elif buttonValue == 1:  # Released
                 pass
-        elif FMS_STATE is State.LAP:
+        elif fms_state == State.LAP:
             led.on()
-            print("\nLap\t->\t")
-            printTime()
+            if not COUNTER_RUNNING:  # Start stopwatch
+                counterCoroObj = uasyncio.create_task(counterCoro())
+                COUNTER_RUNNING = True
+            else:  # Get lap
+                printAddCents()
+                print("\t<-\tLap")
+                printTime()
             if buttonValue == 0:  # Pressed
-                FMS_STATE = State.SHORT_PRESS
+                fms_state = State.SHORT_PRESS
             elif buttonValue == 1:  # Released
-                FMS_STATE = State.IDLE
-        elif FMS_STATE is State.SHORT_PRESS:
+                fms_state = State.IDLE
+        elif fms_state == State.SHORT_PRESS:
             led.on()
             if buttonValue == 0:  # Pressed
                 if (CENTS - startCents) < 0.5*100:
                     pass
                 elif (CENTS - startCents) >= 0.5*100:
-                    FMS_STATE = State.STOP_AND_RESET
+                    fms_state = State.STOP_AND_RESET
             elif buttonValue == 1:  # Released
-                FMS_STATE = State.IDLE
-        elif FMS_STATE is State.STOP_AND_RESET:
+                fms_state = State.IDLE
+        elif fms_state == State.STOP_AND_RESET:
             led.off()  # Quick blink to confirm reset
             await uasyncio.sleep_ms(50)
-            lef.on()
+            led.on()
+            counterCoroObj.cancel()
+            CENTS = 0
+            COUNTER_RUNNING = False
+            print("\nSTOPPED AND RESETTED")
+            print("Restart by pressing BOOT button")
             if buttonValue == 0:  # Pressed
-                FMS_STATE = State.LONG_PRESS
+                fms_state = State.LONG_PRESS
             elif buttonValue == 1:  # Released
-                FMS_STATE = State.IDLE
-        elif FMS_STATE is State.LONG_PRESS:
+                fms_state = State.IDLE
+        elif fms_state == State.LONG_PRESS:
             led.on()
             if buttonValue == 0:  # Pressed
                 pass
             elif buttonValue == 1:  # Released
-                FMS_STATE = State.IDLE
-        await uasyncio.sleep_ms(1)
+                fms_state = State.IDLE
+        await uasyncio.sleep_ms(5)
 
 async def main(buttonPin: Pin, ledPin: Pin) -> None:
     print("Entered main()")
-    uasyncio.create_task(buttonTask(buttonPin, ledPin))
-    await counterCoro()
-    print("Created tasks")
+    uasyncio.create_task(buttonCoro(buttonPin, ledPin))
+    loop = uasyncio.get_event_loop()  # Loop forever, otherwise the program would exit
+    loop.run_forever()
+    print("Created coroutines")
 
 if __name__ == "__main__":
     print("Setting up pins")
